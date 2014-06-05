fd83fbeb922d02b3da5d23a93233426a37e076e3
mdaehnick <mdaehnick@yahoo.com>
2014-06-04 20:39:00 -0400

    updated files so they work

--- calculator.js
+++ calculator.js
@@ -1,40 +1,52 @@
- 
-var currentNumber = 0; // The number currently displayed in the calc
-var decimal = false; // flag - currentNumber already has decimal pt
-var operator = false; // flag - an operator has been pressed
-var currentPos = 10; // tracks ls digit. init value is tenths
-      // as decimals input, moves to hundredths,
-      // thousandths, etc
-var currentSign = 1; // sign (1 = pos, -1 = neg)
+// 'use strict;'
 
+var calculatorState = {
+  currentValue: '',
+  previousValue: '',
+  pendingOperator: ''
+};
 
-function updateDisplay(number) {
-    document.getElementById("display").value = number;
+window.calculatorApp = {
+  clickDigit: function(digit) {
+    if((digit !== 0) || calculatorState.currentValue) {
+      calculatorState.currentValue += digit;
+    }
+    display.value = calculatorState.currentValue;
+  },
+  clickDecimal: function() {
+  },
+  clickOperator: function(op) {
+    if(calculatorState.pendingOperator) {
+      this.clickEquals();
+    }
+    else {
+      calculatorState.previousValue = calculatorState.currentValue;
+    }
+    calculatorState.pendingOperator = op;
+    calculatorState.currentValue = '';
+    display.value = '0';
+  },
+  clickEquals: function() {
+    if(calculatorState.pendingOperator) {
+      var current = parseFloat(calculatorState.currentValue);
+      var prev = parseFloat(calculatorState.previousValue);
+      switch(calculatorState.pendingOperator) {
+        case '+':
+          calculatorState.previousValue = prev + current;
+          break;
+        case '-':
+          calculatorState.previousValue = prev - current;
+          break;
+        case '*':
+          calculatorState.previousValue = prev * current;
+          break;
+        case '/':
+          calculatorState.previousValue = prev / current;
+          break;
       }
-
-function getNumber(inputNum) {
-    operator = false;
-    if (!decimal) {
-          currentNumber *= 10;
-          currentNumber += inputNum * currentSign;
-          } else {
-              currentNumber += inputNum * currentSign / currentPos;
-              currentPos *= 10;
-          }
-          updateDisplay(currentNumber);
-      }
-
-function clearCalc() {
-    currentNumber=0;
-    updateDisplay(currentNumber);
-}
-
-function clearErr() {
-    currentNumber=0;
-    updateDisplay(currentNumber);
-}
-function calcOff () {
-    updateDisplay('');
-    // add code here to dim the display as well
-    //change the "off" button to a power symbol instead, to toggle back on
+      calculatorState.currentValue = '';
+      calculatorState.pendingOperator = '';
+      display.value = calculatorState.previousValue;
+	}
+  }
 }
